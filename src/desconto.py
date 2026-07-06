538913e1b6ec0db5f87fcf0c9232d86f944ad232
Tiago Pereira <tiagojesusp@hotmail.com>
2026-07-06 17:40:31 -0300

    refactor: use dynamic mathematical formula for discount

--- src/desconto.py
+++ src/desconto.py
@@ -1,2 +1,3 @@
 def aplicar_desconto(valor_total, percentual):
-    return valor_total - percentual
+    valor_com_desconto = valor_total * (1 - (percentual / 100))
+    return round(valor_com_desconto, 2)
